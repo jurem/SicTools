6de6f36dc5849e68b3e4b8dbd1e026e25e328ffb
jurem <jure.mihelic@fri.uni-lj.si>
2015-02-05 21:16:29 +0100

    Fix some Timer issues

--- src/sic/sim/Executor.java
+++ src/sic/sim/Executor.java
@@ -14,14 +14,16 @@ public class Executor {
 
     public final Machine machine;
     private Timer timer;
-    private int speedkHz = 10;
+    private int timerPeriod;        // timer period in miliseconds
+    private int timerRepeat;        // timer loop-repeat count
     public final Breakpoints breakpoints;
     public ActionListener onBreakpoint;
     private boolean hasChanged;
 
-    public Executor(Machine machine) {
+    public Executor(final Machine machine) {
         this.machine = machine;
         this.breakpoints = new Breakpoints();
+        setSpeed(10);
     }
 
     public Machine getMachine() {
@@ -29,38 +31,43 @@ public class Executor {
     }
 
     public int getSpeed() {
-        return speedkHz;
+        return 1000 / timerPeriod * timerRepeat;
     }
 
-    public void setSpeed(int kHz) {
-        if (kHz > 10000) kHz = 10000;
-        speedkHz = kHz;
+    public void setSpeed(int Hz) {
+        if (Hz > 1000000) Hz = 10000000;
+        timerRepeat = (Hz + 100) / 100;
+        timerPeriod = 1000 * timerRepeat / Hz;
+    }
+
+    private void timerTick() {
+        for (int i = 0; i < timerRepeat; i++) {
+            int oldPC = machine.registers.getPC();
+            machine.execute();
+            hasChanged = true;
+            // check if the same instruction: halt J halt
+            if (oldPC == machine.registers.getPC()) {
+                stop();
+                break;
+            }
+            // check breakpoints
+            if (breakpoints.has(machine.registers.getPC())) {
+                stop();
+                if (onBreakpoint != null) onBreakpoint.actionPerformed(null);
+                break;
+            }
+        }
     }
 
     public void start() {
         if (timer != null) return;
         timer = new Timer();
-        timer.schedule(new TimerTask() {
+        timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                for (int i = 0; i < speedkHz; i++) {
-                    int oldPC = machine.registers.getPC();
-                    machine.execute();
-                    hasChanged = true;
-                    // check if the same instruction: halt J halt
-                    if (oldPC == machine.registers.getPC()) {
-                        stop();
-                        break;
-                    }
-                    // check breakpoints
-                    if (breakpoints.has(machine.registers.getPC())) {
-                        stop();
-                        if (onBreakpoint != null) onBreakpoint.actionPerformed(null);
-                        break;
-                    }
-                }
+                timerTick();
             }
-        }, 0, 1);
+        }, 0, timerPeriod);
     }
 
     public void stop() {
