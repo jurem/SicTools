jurem/SicTools

commit 890a43ed54e982741e7479161d44ca54cd25a46b

fix a textWriter bug
If data in C or X literal was bigger than max length line, the assembler
wouldn't split it into multiple lines. This commit fixes that.

src/sic/asm/visitors/WriteText.java
@@ -31,9 +31,16 @@ public class WriteText extends WriteVisitor {
 
     private void flushBuf() {
         if (recordBytes == 0) return;
-        w("T%s%06X%s%02X", space, textAddr, space, recordBytes);
-        w(buf.toString());
-        w("\n");
+        String codeString = buf.toString();
+        while (recordBytes > 0) {
+            int length = codeString.length() <= 58 ? codeString.length() / 2 : 29;
+            w("T%s%06X%s%02X", space, textAddr, space, length);
+            w(codeString.substring(0, length * 2));
+            codeString = codeString.substring(length * 2);
+            w("\n");
+            textAddr += length;
+            recordBytes -= length;
+        }
         buf = new StringBuilder();
         recordBytes = 0;
     }

commit 6414a31873c3a0d1ac15731766028a2d1af72e6b

fix T record length bug
This commit attempts to fix two new errors introduced by
commit 890a43ed54e982741e7479161d44ca54cd25a46b.
- Off by one error; maximum length of T record was one byte to short.
- Since splitting longer lines into separate records is done in flush,
  it is no longer necessary to call flush in visit, unless
  explicitly requested by emitText.

src/sic/asm/visitors/WriteText.java
@@ -33,7 +33,7 @@ public class WriteText extends WriteVisitor {
         if (recordBytes == 0) return;
         String codeString = buf.toString();
         while (recordBytes > 0) {
-            int length = codeString.length() <= 58 ? codeString.length() / 2 : 29;
+            int length = codeString.length() <= 60 ? codeString.length() / 2 : 30;
             w("T%s%06X%s%02X", space, textAddr, space, length);
             w(codeString.substring(0, length * 2));
             codeString = codeString.substring(length * 2);
@@ -102,7 +102,7 @@ public class WriteText extends WriteVisitor {
         boolean flush = c.emitText(buf);
         recordBytes += (buf.length() - bufLenBefore) / 2;
 
-        if (flush || recordBytes > 28)
+        if (flush)
             flushBuf();
     }
 