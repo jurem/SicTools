1881dbec08a5d43f9015ba462b0449b89d13376d
Kljunas2 <miha@korenjak.si>
2022-11-10 15:27:07 +0100

    add escape sequences in character strings
    Standard escape sequences (those used in java) and ability to write
    bytes directly (\xbb).

--- src/sic/ast/data/DataChr.java
+++ src/sic/ast/data/DataChr.java
@@ -27,7 +27,51 @@ public class DataChr extends Data {
     public void parse(Parser parser, boolean allowList) throws AsmError {
         parser.advance('C');
         parser.advance('\'');
-        data = parser.readUntil('\'').getBytes();
+        StringBuilder buf = new StringBuilder();
+        for (char c = parser.advance(); c != '\''; c = parser.advance()) {
+            if (c == '\\') {
+                c = parser.advance();
+                switch (c) {
+                    case '\'':
+                        c = '\'';
+                        break;
+                    case '\\':
+                        break;
+                    case 'n':
+                        c = '\n';
+                        break;
+                    case 'r':
+                        c = '\r';
+                        break;
+                    case 't':
+                        c = '\t';
+                        break;
+                    case 'b':
+                        c = '\b';
+                        break;
+                    case 'f':
+                        c = '\f';
+                        break;
+                    case '0':
+                        c = '\0';
+                        break;
+                    case 'x':
+                        int mark = parser.pos();
+                        parser.advance(2);
+                        String str = parser.extract(mark);
+                        try {
+                            c = (char) Integer.parseInt(str, 16);
+                        } catch (NumberFormatException e) {
+                            throw new AsmError(parser.loc(), "Hexadecimal byte expected");
+                        }
+                        break;
+                    default:
+                        throw new AsmError(parser.loc(), "Unknown escape sequence '\\%c'", c);
+                }
+            }
+            buf.append(c);
+        }
+        data = buf.toString().getBytes();
         if (allowList) super.parse(parser, true);
     }
 
